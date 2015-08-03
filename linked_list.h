3ed0c957e59d35f9d0160f5837d8ed6247f5df8e
Amaraj Judge <amaraj@umich.edu>
2015-08-02 23:04:14 -0400

    added testing for isEmpty, insertFront, and removeFront; also wrote implementation for insertFront and constructors

--- linked_list.h
+++ linked_list.h
@@ -1,11 +1,58 @@
-#ifdef
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
 class LinkedList{
+private:
+  struct Node{
+    Node *next;
+    int datum;
+  };
+  //this is the pointer to the beginning of the list
+  Node *head;
 
+  //private member function that will be used by overloading the assignment operator and destructor
+  //written in order to avoid copying code
+  void RemoveAll();
 
+  //used by both overloaded assignment operator and copy constructor
+  Node* CopyAll(const LinkedList &list);
 
+public:
 
+  bool isEmpty();
 
+  void insertFront(int value);
+
+  const int front();
+
+  int removeFront();
+
+  Node* find(int value);
+
+  void deleteElement(int value);
+
+  void deleteList();
+
+  LinkedList();
+
+  LinkedList(int value);
+
+  ~LinkedList();
 };
+
+LinkedList::LinkedList(){
+  this.first = nullptr;
+}
+
+LinkedList::LinkedList(int value){
+  insertFront(value);
+}
+
+void LinkedList::insertFront(int value){
+  Node *np = new Node;
+  np->next = first;
+  np->datum = value;
+  first = np;
+}
 
 #endif
